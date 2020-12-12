11f462a719a025d594dbe9bec81065e0410b3da9
Haruto Tanbe <harutotanabe09@gmail.com>
2020-12-12 11:13:56 +0900

    feat(page): hello.vue fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,32 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">application</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-      <div id="app-6">
-        <p>{{ message }}-text</p>
-      </div>
-    </div>
+    <h1 class="title">{{ title }}</h1>
+    <p>{{ message }}</p>
+    <hr class="hr" />
+    <pre class="pre"> {{ now }}</pre>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
 export default Vue.extend({
   data() {
     return {
-      message: 'hoge',
+      title: 'Hello!',
+      message: 'This is a message',
+      now: 'waiting ...',
     }
+  },
+  created() {
+    const d = new Date()
+    this.now = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
   },
 })
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
+<style type="sass">
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 5px, 10px;
 }
 
 .title {
@@ -60,20 +34,20 @@ export default Vue.extend({
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
   letter-spacing: 1px;
+  font-size: 60pt;
+  color: #345980;
 }
-
+.pre {
+  padding: 10px;
+  font-size: 18px;
+  background-color: #efefef;
+}
 .subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
