d5b0558484967e87ede1cc1f91a8484b8cd0af66
Haruto Tanbe <harutotanabe09@gmail.com>
2020-12-13 11:31:46 +0900

    feat(page): axios page add on

---
+++ components/PostComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="table">
+    <div class="userId">{{ post.userId }}</div>
+    <div class="title">{{ post.title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import Post from '@/types/Post'
+
+export default Vue.extend({
+  name: 'PostComponent',
+  props: {
+    post: {
+      type: Object as PropType<Post>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.table {
+  background: #fff;
+  > .row {
+    display: flex;
+    border-bottom: solid 2px gray;
+    padding: 10px 0;
+    > .title {
+      font-size: 15px;
+      color: #000;
+      width: 30%;
+    }
+  }
+}
+</style>
