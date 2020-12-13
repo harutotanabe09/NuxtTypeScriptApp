5640d312cf4781e45d67b1d4113932b73f449b4d
Haruto Tanbe <harutotanabe09@gmail.com>
2020-12-13 10:36:52 +0900

    feat(page): axios page

---
+++ components/PostIndex.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="table">
+    <div v-for="post in posts" :key="post.id" class="row">
+      <div class="userId">{{ post.userId }}</div>
+      <div class="title">{{ post.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import Post from '@/types/Post'
+
+export default Vue.extend({
+  name: 'PostIndex',
+  props: {
+    posts: {
+      required: true,
+      type: Array as PropType<Post[]>,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.table {
+  background: #fff;
+  > .row {
+    display: flex;
+    border-bottom: solid 2px gray;
+    padding: 10px 0;
+    > .title {
+      font-size: 15px;
+      color: #000;
+      width: 30%;
+    }
+  }
+}
+</style>
---
+++ pages/axios.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+    <PostIndex :posts="posts" class="name" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
+const url = 'https://jsonplaceholder.typicode.com/posts/'
+
+export default Vue.extend({
+  async asyncData() {
+    const rese = await axios.get(url)
+    return {
+      isLoading: false,
+      posts: rese.data,
+    }
+  },
+  data() {
+    return {
+      title: 'Axios!',
+      message: 'essage',
+    }
+  },
+})
+</script>
+
+<style type="sass">
+.container {
+  padding: 5px, 10px;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  letter-spacing: 1px;
+  font-size: 60pt;
+  color: #345980;
+}
+.pre {
+  padding: 10px;
+  font-size: 18px;
+  background-color: #efefef;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.nuxt-link-active {
+  color: red;
+}
+</style>
