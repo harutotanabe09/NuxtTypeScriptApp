f810a7c0aa9c93701c79b97c5a0e0f069ab2b652
Haruto Tanbe <harutotanabe09@gmail.com>
2021-01-30 14:59:30 +0900

    test: 💍 test

--- components/ui/AppTab.vue
+++ components/ui/AppTab.vue
@@ -1,6 +1,6 @@
 <template>
-  <transition name="home">
-    <div class="app-tabs" :class="`-${type}`">
+  <div class="app-tabs" :class="`-${type}`">
+    <transition-group class="slide">
       <div
         v-for="item in items"
         :key="item.label"
@@ -10,8 +10,8 @@
       >
         {{ item.name }}
       </div>
-    </div>
-  </transition>
+    </transition-group>
+  </div>
 </template>
 
 <script lang="ts">
@@ -44,42 +44,38 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.home-enter-active,
-.home-leave-active {
-  transition: opacity 0.5s;
-}
-.home-enter,
-.home-leave-active {
-  opacity: 0;
-}
-
 .app-tabs {
   display: flex;
-  > .item {
-    padding: 22px 10px;
-    margin: 0 18px;
-    text-align: center;
-    &.-active {
-      color: $text-app-tab;
-      border-bottom: $border-style 3px $border-app-tab;
-      box-shadow: $box-shadow-app-tab;
-    }
-  }
-  &.-block {
-    height: $app-tab-block-height;
-    background-color: $bg-app-tab-block;
-    border-radius: $app-tab-block-height / 2;
-    box-shadow: $box-shadow-app-tab-block;
+  > .slide {
+    display: flex;
     > .item {
-      width: 140px;
-      padding: 12px 0;
-      margin: 0;
-      line-height: $lh-12;
+      padding: 22px 10px;
+      margin: 0 18px;
+      text-align: center;
+      transition-timing-function: ease-out;
+      transition-duration: 1s;
       &.-active {
-        color: $text-app-button-primary;
-        background: $bg-app-button-primary;
-        border-radius: $app-tab-block-height / 2;
+        color: $text-app-tab;
+        border-bottom: $border-style 3px $border-app-tab;
         box-shadow: $box-shadow-app-tab;
+      }
+    }
+    &.-block {
+      height: $app-tab-block-height;
+      background-color: $bg-app-tab-block;
+      border-radius: $app-tab-block-height / 2;
+      box-shadow: $box-shadow-app-tab-block;
+      > .item {
+        width: 140px;
+        padding: 12px 0;
+        margin: 0;
+        line-height: $lh-12;
+        &.-active {
+          color: $text-app-button-primary;
+          background: $bg-app-button-primary;
+          border-radius: $app-tab-block-height / 2;
+          box-shadow: $box-shadow-app-tab;
+        }
       }
     }
   }
